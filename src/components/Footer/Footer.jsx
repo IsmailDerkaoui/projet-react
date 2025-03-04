f421fa9a1739477ece1c04b170927d1d73d34f3b
IsmailDerkaoui <ismailderkaoui.0@gmail.com>
2025-03-04 01:16:28 +0100

    création du footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./footer.scss"
+
+function Footer() {
+    return <div className="footer">
+    <img src="./img/logo-footer.png" alt="logoFooter" className="imgFooter" />
+    <p className="textFooter">2020 Kasa. All rights reserved</p>
+    </div>
+  }
+  export default Footer;
