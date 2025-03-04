e94c02da982b48cf46d687013e4a56925827a6a9
IsmailDerkaoui <ismailderkaoui.0@gmail.com>
2025-03-04 01:16:52 +0100

    mise a jour de la navbar

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,14 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./navbar.scss"
 
 function Navbar() {
     return <nav className="navbar">
+      <Link to="/">
       <div className="navbar-logo">
       <img src="./img/logo.png" alt="logo kasa" />
       </div>
-      <div className="link-navbar">
-      <div>Accueil </div>
-      <div>A Propos</div>
+      </Link>
+      <div className="linkNavbar">
+      <Link to="/" >Accueil </Link>
+      <Link to="/about" >A Propos</Link>
       </div>
     </nav>;
   }
