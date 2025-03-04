57c330ebbaea230c16eb39863b4ce2e9c1c44a30
IsmailDerkaoui <ismailderkaoui.0@gmail.com>
2025-03-04 01:15:08 +0100

    création de la banner avec le css

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import "./banner.scss"
 
 function Banner() {
-    return <div className="banner">
-    <nav>
-      <img src="./img/banner.png" alt="bannière kasa" className="banner-img"/>
+    return <div >
+    <nav className="banner">
+      <img src="./img/banner.png" alt="bannière kasa" className="bannerImg"/>
     </nav>
     </div>
   }
