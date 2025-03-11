c247140a85e1461269002885f54fef1a34760722
IsmailDerkaoui <ismailderkaoui.0@gmail.com>
2025-03-11 01:04:05 +0100

    maj des cards

--- src/components/Cards/Logement.jsx
+++ src/components/Cards/Logement.jsx
@@ -8,7 +8,10 @@ const logement = props.logement
   return (
     <Link to={"/apartments/"+ logement.id} className="logement-link">
     <div key={logement.id} className="logement-card">
+    <div className="logement-image-container">
       <img src={logement.cover} alt={logement.title} className="logement-image" />
+      <div className="logement-overlay"></div>
+      </div>
       <h3 className="logement-title">{logement.title}</h3>
     </div>
 </Link>
