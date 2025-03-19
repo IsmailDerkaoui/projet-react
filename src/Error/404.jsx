836688264a0cfb43720b29c6bbd5ac0d367edc16
IsmailDerkaoui <ismailderkaoui.0@gmail.com>
2025-03-19 02:12:45 +0100

    maj de la page d'erreur 404

--- src/Error/404.jsx
+++ src/Error/404.jsx
@@ -1,26 +1,26 @@
 import React from "react";
 //import "./App.css"
-import Navbar from "../components/nav/Navbar.jsx"
-import Footer from "../components/Footer/Footer.jsx"
-import "./Error.scss"
+import Navbar from "../components/nav/Navbar.jsx";
+import Footer from "../components/Footer/Footer.jsx";
+import "./Error.scss";
 import { Link } from "react-router-dom";
 //import ErrorImg from "./components/Error/ErrorImg.jsx"
-
 
 function Error404() {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
       <div className="Error">
-      <img src="./404.png" alt="Erreur 404" className="imgError"/>
-      <p className="textError">Oups ! La page que vous demandez n'existe pas.</p>
-      <Link to = "/">
-      <p className="retourAccueil">Retourner sur la page d'accueil</p>
-      </Link>
-      
+        <img src="./404.png" alt="Erreur 404" className="imgError" />
+        <p className="textError">
+          Oups ! La page que vous demandez n'existe pas.
+        </p>
+        <Link to="/">
+          <p className="retourAccueil">Retourner sur la page d'accueil</p>
+        </Link>
       </div>
-      <Footer/>
+      <Footer />
     </div>
-  )
+  );
 }
 export default Error404;
